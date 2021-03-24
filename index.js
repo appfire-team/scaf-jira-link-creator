268bdc845b5c0e8f72a366b5aa8488f17f8d9181
Ahmed Alaghbari <ahmed.alaghbari@servicerocket.com>
2021-03-25 01:17:02 +0800

    [SCAF-3448] Add log files and summary.
    * Add logger for INFO and ERROR logs.
    * Save log output to log files.
    * Print summary at the end of the execution.
    * Add more details to README.md.

--- index.js
+++ index.js
@@ -1,103 +1,118 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const axios = require('axios');
-const path = require('path');
-const csv = require('fast-csv');
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+import csv from 'fast-csv';
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+import Logger from './logger.js';
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
+const {log, logSummary, LOG_LEVELS: {INFO, ERROR}} = Logger;
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const argv = yargs(hideBin(process.argv))
-    .option("jira-url", {
-        type: "string",
-        describe: "Jira base url"
-    })
-    .option("conf-url", {
-        type: "string",
-        describe: "Confluence base url"
-    })
-    .option("csv", {
-        type: "string",
-        describe: "CSV filename in the current folder"
-    })
-    .option("app-id", {
-        type: "string",
-        describe: "AppID can be obtained from the Application Links menu in Jira (Click on pencil icon and ID is in the URL)"
-    })
-    .option("jira-auth", {
-        type: "string",
-        describe: "Username and password for jira in the following format \"username:password\""
-    })
-    .option("dry-run", {
-        type: "boolean",
-        default: false,
-        describe: "Script will not submit HTTP requests, if this flag is enabled."
-    })
-    .demandOption(["jiraUrl", "confUrl", "csv", "appId"])
-    .argv;
+  .option("jira-url", {
+    type: "string",
+    describe: "Jira base url"
+  })
+  .option("conf-url", {
+    type: "string",
+    describe: "Confluence base url"
+  })
+  .option("csv", {
+    type: "string",
+    describe: "CSV filename in the current folder"
+  })
+  .option("app-id", {
+    type: "string",
+    describe: "AppID can be obtained from the Application Links menu in Jira (Click on pencil icon and ID is in the URL)"
+  })
+  .option("jira-auth", {
+    type: "string",
+    describe: "Username and password for the Jira user with 'Link Issues' permission in the following format 'username:password'"
+  })
+  .option("dry-run", {
+    type: "boolean",
+    default: false,
+    describe: "Script will not submit HTTP requests, if this flag is enabled."
+  })
+  .demandOption(["jira-url", "conf-url", "csv", "app-id", "jira-auth"])
+  .argv;
 
 const ISSUE_KEY_REGEX = /(?<=key&quot;&gt;)[A-Z]+-[0-9]+(?=&lt;)/g;
 
 const jiraEndpoint = argv['jira-url'];
 const confluenceEndpoint = argv['conf-url'];
-const csvFilename = argv.csv;
+const csvFilename = argv['csv'];
 const appId = argv['app-id'];
 const usernameAndPassword = argv['jira-auth'];
 const isDryRun = argv['dry-run'];
 
+const authBuffer = Buffer.from(usernameAndPassword, 'utf-8');
+const requests = [];
+let createdLinksCount = 0;
+let preexistingLinksCount = 0;
+let errCount = 0;
+
 const processRow = row => {
 
-    const pageId = row["Page ID"];
-    const title = row["Page Title"];
-    const metadata = row["text_val"];
+  const pageId = row["Page ID"];
+  const title = row["Page Title"];
+  const metadata = row["text_val"];
 
-    const issueKeys = metadata.match(ISSUE_KEY_REGEX);
-    console.log(`[PAGE-ID: ${pageId}] Keys: ${issueKeys}`);
+  const issueKeys = metadata.match(ISSUE_KEY_REGEX);
+  log(INFO, `[PAGE-ID: ${pageId}] Keys: ${issueKeys}`);
 
-    const body = {
-        globalId: `appId=${appId}&pageId=${pageId}`,
-        application: {
-            type: "com.atlassian.confluence",
-            name: "Confluence"
-        },
-        relationship: "mentioned in",
-        object: {
-            url: `${confluenceEndpoint}/pages/viewpage.action?pageId=${pageId}`,
-            title
+  const body = {
+    globalId: `appId=${appId}&pageId=${pageId}`,
+    application: {
+      type: "com.atlassian.confluence",
+      name: "Confluence"
+    },
+    relationship: "mentioned in",
+    object: {
+      url: `${confluenceEndpoint}/pages/viewpage.action?pageId=${pageId}`,
+      title
+    }
+  };
+
+  issueKeys.forEach(issueKey => {
+    const requestUrl = `${jiraEndpoint}/rest/api/latest/issue/${issueKey}/remotelink`;
+
+    if (isDryRun) {
+      log(INFO, `[DRY-RUN] Will update ${requestUrl}`);
+      return;
+    }
+
+    requests.push(
+      axios.post(requestUrl, body, {
+        headers: {
+          'Authorization': `Basic ${authBuffer.toString('base64')}`,
+          'Content-Type': 'application/json'
         }
-    };
-
-    issueKeys.forEach(async issueKey => {
-        const requestUrl = `${jiraEndpoint}/rest/api/latest/issue/${issueKey}/remotelink`;
-
-        const authBuffer = Buffer.from(usernameAndPassword, 'utf-8');
-
-        if (isDryRun) {
-            console.log(`[DRY-RUN] Will update ${requestUrl}`);
-            return;
-        }
-
-        await axios.post(requestUrl, body, {
-            headers: {
-                'Authorization': `Basic ${authBuffer.toString('base64')}` 
-            }
-        })
+      })
         .then(result => {
-            console.log(`Update success: ${requestUrl} => ${result.status}`, result.data);
+          result.status === 201 ? createdLinksCount++ : preexistingLinksCount++;
+          log(INFO, `Update success: ${requestUrl} => ${result.status} ${JSON.stringify(result.data)}`);
         })
         .catch(error => {
-            if (error.response) {
-                console.error(`[ERROR] ${requestUrl} => ${error.response.status}`, error.response.data);
-            }
-        });
-    });
+          errCount++
+          log(ERROR, JSON.stringify(body.object));
+          if (error.response) {
+            log(ERROR, `[ERROR] ${requestUrl} => ${error.response.status}`);
+          }
+        })
+    )
+  });
 };
 
 fs.createReadStream(path.resolve(__dirname, csvFilename))
-    .pipe(csv.parse({ headers: true }))
-    .on("error", console.error)
-    .on("data", processRow)
-    .on("end", async rowCount => {
-        console.log(`Parsed ${rowCount} rows`);
-    });
+  .pipe(csv.parse({headers: true}))
+  .on("error", console.error)
+  .on("data", processRow)
+  .on("end", async rowCount => {
+    log(INFO, `Parsed ${rowCount} rows`);
+    await Promise.allSettled(requests);
+    logSummary(rowCount, requests.length, createdLinksCount, preexistingLinksCount, errCount, isDryRun);
+  });
